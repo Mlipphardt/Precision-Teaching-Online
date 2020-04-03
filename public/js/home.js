31584f17bcf1d5d76340b49d8ad0c60e874d7374
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-03 16:29:00 -0400

    API routes created and tested

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,13 @@
-let occupations = [
-  "./assets/images/mechanic.jpg",
-  "./assets/images/nurse.jpg",
-  "./assets/images/gardener.jpg",
-  "./assets/images/pilot.jpg"
-];
-let kitchenitems = ["./"];
-let lastImage;
+var items = [];
+var lastImage;
+console.log(items);
 
 function nextImage() {
   imageRoll = Math.floor(Math.random() * occupations.length);
   if (imageRoll === lastImage) {
     nextImage();
   }
-  let nextImage = occupations[lastImage];
+  var nextImage = occupations[lastImage];
   console.log(nextImage);
   $("#operant-display").attr("src", nextImage);
   lastImage = imageRoll;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,103 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  //Get all clients
+  app.get("/api/clients", function(req, res) {
+    db.Client.findAll({}).then(function(dbClients) {
+      res.json(dbClients);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  //Find one client by id, include programs
+  app.get("/api/client/:clientid", function(req, res) {
+    db.Client.findOne({
+      include: [{ model: db.Program }],
+      where: { id: req.params.clientid }
+    }).then(function(dbPrograms) {
+      res.json(dbPrograms);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new client
+  app.post("/api/clients", function(req, res) {
+    db.Client.create(req.body).then(function(dbClients) {
+      res.json(dbClients);
+    });
+  });
+
+  // Delete client by id
+  app.delete("/api/client/:id", function(req, res) {
+    db.Example.destroy({
+      where: { id: req.params.id }
+    }).then(function(dbClients) {
+      res.json(dbClients);
+    });
+  });
+
+  //Get all programs
+  app.get("/api/programs", function(req, res) {
+    db.Program.findAll({}).then(function(dbPrograms) {
+      res.json(dbPrograms);
+    });
+  });
+
+  //Get all programs for a specific user
+  app.get("/api/programs/client/:clientid", function(req, res) {
+    db.Program.findAll({
+      include: [{ model: db.Resource }],
+      where: { ClientId: req.params.clientid }
+    }).then(function(dbPrograms) {
+      res.json(dbPrograms);
+    });
+  });
+
+  //Find one program by id, include resources
+  app.get("/api/programs/:programid", function(req, res) {
+    db.Program.findOne({
+      include: [{ model: db.Resource }],
+      where: { ClientId: req.params.programid }
+    }).then(function(dbPrograms) {
+      res.json(dbPrograms);
+    });
+  });
+
+  // Create a new program
+  app.post("/api/programs", function(req, res) {
+    db.Program.create(req.body).then(function(dbPrograms) {
+      res.json(dbPrograms);
+    });
+  });
+
+  // Delete program by id
+  app.delete("/api/program/:id", function(req, res) {
+    db.Program.destroy({
+      where: { id: req.params.id }
+    }).then(function(dbPrograms) {
+      res.json(dbPrograms);
+    });
+  });
+
+  //Get all resources
+
+  app.get("/api/resources", function(req, res) {
+    db.Resource.findAll({}).then(function(dbResources) {
+      res.json(dbResources);
+    });
+  });
+
+  // Create a new resource
+  app.post("/api/resources", function(req, res) {
+    db.Resource.create(req.body).then(function(dbResources) {
+      res.json(dbResources);
+    });
+  });
+
+  // Delete resource by id
+  app.delete("/api/resources/:id", function(req, res) {
+    db.Resource.destroy({
+      where: { id: req.params.id }
+    }).then(function(dbResource) {
+      res.json(dbResource);
     });
   });
 };
