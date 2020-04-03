1501210d21124eba8eff9d3a1da981676f2fa9ab
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-03 16:52:28 -0400

    Client buttons now displayed on page load

--- public/js/home.js
+++ public/js/home.js
@@ -13,4 +13,20 @@ function nextImage() {
   lastImage = imageRoll;
 }
 
+function populateClients() {
+  $.get("/api/clients").then(function(data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var newClient = $(
+        "<button class = 'btn bg-success mx-3 mb-5 client-button'> " +
+          data[i].name +
+          "</button>"
+      );
+      $("#client-list").append(newClient);
+    }
+  });
+}
+
+populateClients();
+
 $("#nextImage").on("click", nextImage);
